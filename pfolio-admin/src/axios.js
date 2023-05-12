388d6f359d526ef6d9e0b9c799c9380395561dea
bubenchikus <dozythoth@yandex.ru>
2023-05-12 10:39:53 +0300

    Preparing production settings

--- pfolio-admin/src/axios.js
+++ pfolio-admin/src/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+import dotenv from "dotenv";
+dotenv.config();
+
 const instance = axios.create({
-  baseURL: "http://localhost:4444",
+  // baseURL: "http://localhost:4444",
+  baseUrl: process.env.REACT_APP_BACK_URL,
 });
 
 export default instance;
