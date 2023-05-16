a1e41314f4eaa3bae4d656aa320060a7a7b79cc1
bubenchikus <dozythoth@yandex.ru>
2023-05-17 01:50:02 +0300

    removed /api path (it's unnecessary after creating separate hostname for api)

--- pfolio-backend/index.js
+++ pfolio-backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 app.use(cors());
 
-app.use("/api/pictures", express.static("pictures"));
+app.use("/pictures", express.static("pictures"));
 
 const storage = multer.diskStorage({
   destination: (_, __, cb) => {
@@ -31,95 +31,84 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-app.post(`/api/upload`, checkAuth, upload.single("image"), (req, res) => {
+app.post(`/upload`, checkAuth, upload.single("image"), (req, res) => {
   res.json({ message: "Image succesfully uploaded" });
 });
 
-app.post(
-  `/api/upload-preview`,
-  checkAuth,
-  upload.single("image"),
-  (req, res) => {
-    PictureController.placePreview(req.file.originalname);
-    res.json({ message: "Image succesfully uploaded" });
-  }
-);
+app.post(`/upload-preview`, checkAuth, upload.single("image"), (req, res) => {
+  PictureController.placePreview(req.file.originalname);
+  res.json({ message: "Image succesfully uploaded" });
+});
 
-app.get("/api/art/cg-paint-left", PictureController.getCGPaintLeft);
-app.get("/api/art/cg-paint-right", PictureController.getCGPaintRight);
-app.get("/api/art/cg-graph", PictureController.getCGGraph);
-app.get("/api/art/trad", PictureController.getTrad);
-app.get("/api/art/comics", PictureController.getComics);
+app.get("/art/cg-paint-left", PictureController.getCGPaintLeft);
+app.get("/art/cg-paint-right", PictureController.getCGPaintRight);
+app.get("/art/cg-graph", PictureController.getCGGraph);
+app.get("/art/trad", PictureController.getTrad);
+app.get("/art/comics", PictureController.getComics);
 
+app.get("/pages-descriptions", DescriptionController.getAllPagesDescriptions);
 app.get(
-  "/api/pages-descriptions",
-  DescriptionController.getAllPagesDescriptions
-);
-app.get(
-  "/api/pages-descriptions/:page",
+  "/pages-descriptions/:page",
   DescriptionController.getPageDescriptionByTitle
 );
 app.post(
-  "/api/pages-descriptions",
+  "/pages-descriptions",
   checkAuth,
   DescriptionController.uploadPageDescription
 );
 app.patch(
-  "/api/pages-descriptions/:id",
+  "/pages-descriptions/:id",
   checkAuth,
   DescriptionController.updatePageDescription
 );
 app.delete(
-  "/api/pages-descriptions/:id",
+  "/pages-descriptions/:id",
   checkAuth,
   DescriptionController.deletePageDescription
 );
 
-app.get(
-  "/api/series-descriptions",
-  DescriptionController.getAllSeriesDescriptions
-);
+app.get("/series-descriptions", DescriptionController.getAllSeriesDescriptions);
 app.post(
-  "/api/series-descriptions",
+  "/series-descriptions",
   checkAuth,
   DescriptionController.uploadSeriesDescription
 );
 app.get(
-  "/api/series-descriptions/:category",
+  "/series-descriptions/:category",
   DescriptionController.getSeriesDescriptionsByCategory
 );
 app.patch(
-  "/api/series-descriptions/:id",
+  "/series-descriptions/:id",
   checkAuth,
   DescriptionController.updateSeriesDescription
 );
 app.delete(
-  "/api/series-descriptions/:id",
+  "/series-descriptions/:id",
   checkAuth,
   DescriptionController.deleteSeriesDescription
 );
 
-app.get("/api/pictures", PictureController.getUnhiddenPictures);
+app.get("/pictures", PictureController.getUnhiddenPictures);
 app.post(
-  "/api/pictures",
+  "/pictures",
   pictureValidation,
   validationResultStatus,
   checkAuth,
   PictureController.uploadPicture
 );
-app.patch("/api/pictures/:id", checkAuth, PictureController.updatePicture);
-app.delete("/api/pictures/:id", checkAuth, PictureController.deletePicture);
+app.patch("/pictures/:id", checkAuth, PictureController.updatePicture);
+app.delete("/pictures/:id", checkAuth, PictureController.deletePicture);
 
-app.get("/api/all-pictures", checkAuth, PictureController.getAllPictures);
+app.get("/all-pictures", checkAuth, PictureController.getAllPictures);
 
-app.get("/api/posts", PostController.getAllPosts);
-app.post("/api/posts", checkAuth, PostController.uploadPost);
-app.patch("/api/posts/:id", checkAuth, PostController.updatePost);
-app.delete("/api/posts/:id", checkAuth, PostController.deletePost);
+app.get("/posts", PostController.getAllPosts);
+app.post("/posts", checkAuth, PostController.uploadPost);
+app.patch("/posts/:id", checkAuth, PostController.updatePost);
+app.delete("/posts/:id", checkAuth, PostController.deletePost);
 
-app.post("/api/login", AdminController.login);
+app.post("/login", AdminController.login);
 
-app.get("/api/columns", checkAuth, AdminController.getColumns);
+app.get("/columns", checkAuth, AdminController.getColumns);
 
 PictureController.maintainPictureDB();
 
