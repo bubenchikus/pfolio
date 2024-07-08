9d5a32fda96e87087e3f7b1ae66e4ee439947262
bubenchikus <dozythoth@yandex.ru>
2024-07-09 00:19:45 +0300

    Try to fix mobile scrolling 6

--- frontend/src/pages/Journal.jsx
+++ frontend/src/pages/Journal.jsx
@@ -50,7 +50,7 @@ const Journal = () => {
 
     const handleScroll = () => {
       if (
-        window.innerHeight + document.documentElement.scrollTop ===
+        window.innerHeight + Math.ceil(document.documentElement.scrollTop) ===
         document.documentElement.offsetHeight
       ) {
         fetchData();
